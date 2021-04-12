4272c023870c1c1050145335149b5b90af7b9502
LynnLululu <luyiqun130@foxmail.com>
2021-04-12 11:23:45 -0500

    movement & basic interaction

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float speed = 3.0F;
-    public float rotateSpeed = 3.0F;
     public float gravity = 10.0F;
-    private Vector3 moveDirection = Vector3.zero;
     CharacterController controller;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -16,9 +15,20 @@ public class Character : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        float curSpeed = speed * Input.GetAxis("Vertical");
-        controller.SimpleMove(forward * curSpeed);
+        playerMovement();
+    }
+
+    //requirement: keyboards doesn't change the angle of the horizontal movement, 
+    //Instead, we use camera(rectile pointer) to enable player to move horizontally in any angle
+    void playerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        //bind velocity with camera
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y = 0;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
