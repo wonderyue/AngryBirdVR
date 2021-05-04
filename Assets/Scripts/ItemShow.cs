aff05e4b6001265aeae71e813c14a7b2670ecf25
LynnLululu <luyiqun130@foxmail.com>
2021-05-04 03:12:08 -0500

    add door code hint only show when Light1 off

---
+++ Assets/Scripts/ItemShow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemShow : MonoBehaviour
+{
+    public GameObject item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showItem()
+    {
+        item.SetActive(true);
+    }
+}
