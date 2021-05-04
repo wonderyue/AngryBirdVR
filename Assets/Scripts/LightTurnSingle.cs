c37b4bc1547dee5be4934ae1a8af46f27f08aef9
LynnLululu <luyiqun130@foxmail.com>
2021-05-03 23:36:52 -0500

    Set Room1

---
+++ Assets/Scripts/LightTurnSingle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTurnSingle : MonoBehaviour
+{
+    public GameObject light;
+
+    public void LightOn()
+    {
+        {
+            if (light.activeSelf)
+            {
+                light.SetActive(false);
+            }
+            else
+            {
+                light.SetActive(true);
+            }
+        }
+    }
+}
